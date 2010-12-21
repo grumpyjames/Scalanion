62a71d2650c7afae2ecb0e031f5b0971b95eee72
James Byatt <grumpyjames@gmail.com>
2010-12-21 13:30:43 +0000

    bit of reformatting

--- src/main/scala/org/grumpysoft/CommandLinePlayer.scala
+++ src/main/scala/org/grumpysoft/CommandLinePlayer.scala
@@ -2,15 +2,19 @@ package org.grumpysoft
 
 class CommandLinePlayer(private val name: String, private val userInterface: Promptable) extends Player with SelfDescribing {
 
+  def describe() : String = {
+    "player named " + name
+  }
+
   def chooseFrom(cards: Seq[Card], purpose: Verb, minChoices: Int, maxChoices: Int) : Seq[Int] = {
     nextInput(buildDescription(purpose, minChoices, maxChoices), cards).dropWhile(invalid(minChoices, maxChoices, _)).head
   }
 
-  def nextInput(greeting: SelfDescribing, options: Seq[SelfDescribing]) : Stream[Seq[Int]] = {
+  private def nextInput(greeting: SelfDescribing, options: Seq[SelfDescribing]) : Stream[Seq[Int]] = {
     Stream.cons(userInterface.prompt(greeting, options), nextInput(greeting, options))
   }
 
-  def invalid(minChoices: Int, maxChoices: Int, choices: Seq[Int]) : Boolean = {
+  private def invalid(minChoices: Int, maxChoices: Int, choices: Seq[Int]) : Boolean = {
     println("min: " + minChoices + ", max: " + maxChoices + ", choices: " + choices)
     choices.size match {
       case x if x < minChoices => true
@@ -19,7 +23,7 @@ class CommandLinePlayer(private val name: String, private val userInterface: Pro
     }
   }
 
-  def buildDescription(purpose: Verb, minChoices: Int, maxChoices: Int) : SelfDescribing = {
+  private def buildDescription(purpose: Verb, minChoices: Int, maxChoices: Int) : SelfDescribing = {
     if (minChoices == maxChoices && minChoices > 0)
       description("Choose", minChoices, purpose)
     else if (minChoices == 0)
@@ -28,24 +32,18 @@ class CommandLinePlayer(private val name: String, private val userInterface: Pro
       error("don't know how to build this description")
   }
 
-  def description(prefix: String, choiceCount: Int, purpose: Verb) : SelfDescribing = {
+  private def description(prefix: String, choiceCount: Int, purpose: Verb) : SelfDescribing = {
     QuickDescription(prefix + " " + pluralise(choiceCount) + " to " + purpose.present)
   }
 
-  def pluralise(num: Int) : String = num match {
+  private def pluralise(num: Int) : String = num match {
     case 1 => "1 card"
     case a => a.toString + " cards"
   }
 
-  
-
-  def describe() : String = {
-    "player named " + name
-  }
-
 }
 
-case class QuickDescription(val desc: String) extends SelfDescribing {
+private case class QuickDescription(val desc: String) extends SelfDescribing {
   def describe() : String = { desc }
 }
 
