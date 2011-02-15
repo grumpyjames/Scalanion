aa8ac525db35be09587872bde9ddecee24631da8
James Byatt <grumpyjames@gmail.com>
2011-02-15 23:10:58 +0000

    move another test over to specs

---
+++ scalanion/src/test/scala/org/grumpysoft/OptionFormatterSpec.scala
@@ -0,0 +1,13 @@
+package org.grumpysoft
+
+import org.specs.Specification
+
+object OptionFormatterSpec extends Specification {
+  val unformatted = List(StringDescription("forum"),StringDescription("quorum"),StringDescription("no"))
+  "an option formatter when asked to format some options" should {
+    "number and format them nicely" in {
+      val formatted = new OptionFormatter().format(unformatted).map(_.describe)
+      formatted must_==List("1. forum", "2. quorum", "3. no")
+    }
+  }
+}
