f3f91255610ae50cacf4703fb86f636e2b13c18b
James Byatt <grumpyjames@gmail.com>
2011-02-15 23:08:45 +0000

    start migrating some old tests to use specs rather than scala test

---
+++ scalanion/src/test/scala/org/grumpysoft/RichPlayerSpec.scala
@@ -0,0 +1,32 @@
+package org.grumpysoft
+
+import actioncards.Militia
+import org.specs.Specification
+import ActionCards._
+import VictoryCards._
+
+
+object RichPlayerSpec extends Specification {
+  class FakePlayer extends GenericPlayer[Int] {
+    def chooseFrom(cards: Seq[Card], purpose: Verb, minChoices: Int, maxChoices: Int) : Seq[Int] = {
+      purpose must_==Discard
+      minChoices must_==2
+      maxChoices must_==2
+      List(1,2)
+    }
+
+    def query(question: Query) = true
+    def newHand(hand: Seq[Card]) : Unit = {}
+    def playerEvent(player: GenericPlayer[Any], action: Verb, cards: Seq[Card]) : Unit = {}
+    def describe() : String = {""}
+  }
+
+  "when prompted, rich player" should {
+    "upcast responses back to rich types" in {
+      val richie = new RichPlayer(new FakePlayer)
+      val cards = List(Witch(), Militia(), Province())
+      richie.chooseFrom(cards, Discard, 2, 2) must_==cards.take(2)
+    }
+  }
+
+}
