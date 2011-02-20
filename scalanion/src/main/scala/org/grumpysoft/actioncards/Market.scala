0e6fdcf547e00c80153d40fd18c11f26d25f6a0b
James Byatt <grumpyjames@gmail.com>
2011-02-20 22:47:44 +0000

    move market out into real source area

---
+++ scalanion/src/main/scala/org/grumpysoft/actioncards/Market.scala
@@ -0,0 +1,18 @@
+package org.grumpysoft.actioncards
+
+import org.grumpysoft._
+
+object Market {
+  def apply() : Market = { new Market }
+}
+
+class Market extends ActionCard(5) {
+  def play(stacks: Stacks, player: GenericPlayer[Card], supply: Supply, table: Table) : ActionResult = {
+    ActionResult(1, 1, 1, stacks.addCards(1), supply, table)
+  }
+
+  def describe() = "Market"
+}
+
+
+
--- scalanion/src/test/scala/org/grumpysoft/actioncards/MarketSpec.scala
+++ scalanion/src/test/scala/org/grumpysoft/actioncards/MarketSpec.scala
@@ -18,15 +18,3 @@ object MarketSpec extends ActionCardSpecBase {
 
 
 }
-
-object Market {
-  def apply() : Market = { new Market }
-}
-
-class Market extends ActionCard(5) {
-  def play(stacks: Stacks, player: GenericPlayer[Card], supply: Supply, table: Table) : ActionResult = {
-    ActionResult(1, 1, 1, stacks.addCards(1), supply, table)
-  }
-
-  def describe() = "Market"
-}
