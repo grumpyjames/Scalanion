bb510d3c8da3a93fba23a97bc18c379de1c8c13e
James Byatt <grumpyjames@gmail.com>
2011-03-06 18:36:54 +0000

    fiddle with protobuf schema, add events and hand transmission, done for now...

--- scalanion/src/test/scala/org/grumpysoft/NetworkedPlayerSpec.scala
+++ scalanion/src/test/scala/org/grumpysoft/NetworkedPlayerSpec.scala
@@ -4,27 +4,44 @@ import org.specs.Specification
 import scalaj.collection.Imports._
 import java.io.{OutputStream, ByteArrayInputStream, InputStream, ByteArrayOutputStream}
 import org.grumpysoft.TreasureCards.{Copper, Silver}
-import org.grumpysoft.Scalanion.{Query => ProtobufQuery, ChooseFrom, Answer, Choices}
+import org.specs.mock.Mockito
+import org.grumpysoft.Scalanion.{Hand, Query => ProtobufQuery, ChooseFrom, ChooseForOtherPlayer => ProtobufChooseForOtherPlayer, Answer, Choices, Event}
 
-object NetworkedPlayerSpec extends Specification {
-  val output = new ByteArrayOutputStream(2048)
-  val input = buildResponsesInput()
+object NetworkedPlayerSpec extends Specification with Mockito {
+
+  val mockOtherPlayer = mock[GenericPlayer[Any]]
+
+  def buildInput(mutator: (OutputStream => Unit)) : InputStream = {
+    val scratchSpace = new ByteArrayOutputStream(128)
+    mutator(scratchSpace)
+    toInput(scratchSpace)
+  }
 
   def buildResponsesInput() : InputStream = {
-    val scratchSpace = new ByteArrayOutputStream(128)
-    Choices.newBuilder.addChoice(0).addChoice(3).build.writeDelimitedTo(scratchSpace)
-    Answer.newBuilder.setAnswer(true).build.writeDelimitedTo(scratchSpace)
-    toInput(scratchSpace)
+    buildInput({scratchSpace =>
+      Choices.newBuilder.addChoice(0).addChoice(3).build.writeDelimitedTo(scratchSpace)
+      Answer.newBuilder.setAnswer(true).build.writeDelimitedTo(scratchSpace)
+    })
+  }
+
+  def falseResponseInput() : InputStream = {
+    buildInput(scratchSpace => Answer.newBuilder.setAnswer(false).build.writeDelimitedTo(scratchSpace))
   }
 
   def toInput(baos: ByteArrayOutputStream) : InputStream = {
     new ByteArrayInputStream(baos.toByteArray)
   }
 
-  val networkPlayer = NetworkPlayer(input, output)
+  def noInput() : InputStream = {
+    buildInput(ss => {})
+  }
 
   "network player" should {
-    "transmit choices correctly, and retrieve the response" in {
+    val output = new ByteArrayOutputStream(2048)
+
+    "transmit choices and retrieve responses" in {
+      val networkPlayer = NetworkPlayer(buildResponsesInput, output)
+
       networkPlayer.chooseFrom(List(Copper(), Silver(), Silver(), Copper()), Discard, 0, 2) must_==List(0, 3)
       networkPlayer.query(BasicQuestion("are you geoff?")) must_==true
 
@@ -38,6 +55,33 @@ object NetworkedPlayerSpec extends Specification {
       val transmittedQuery = ProtobufQuery.parseDelimitedFrom(networkPlayerOutput)
       transmittedQuery.getQuestion must_=="are you geoff?"
     }
+
+    "transmit queries corresponding to choices for other players" in {
+      val networkPlayer = NetworkPlayer(falseResponseInput, output)
+      mockOtherPlayer.describe returns "another player"
+      networkPlayer.query(ChooseForOtherPlayer(List(Copper(), Silver()), mockOtherPlayer, Gain)) must_==false
+      val transmitted = ProtobufChooseForOtherPlayer.parseDelimitedFrom(toInput(output))
+      transmitted.getPlayer must_=="another player"
+      transmitted.getVerb must_=="gain"
+      transmitted.getCardList.asScala must_==List("Copper", "Silver")
+    }
+
+    "transmit hand contents" in {
+      val networkPlayer = NetworkPlayer(noInput, output)
+      networkPlayer.newHand(List(Copper(), Silver(), Copper()))
+      val transmitted = Hand.parseDelimitedFrom(toInput(output))
+      transmitted.getCardList.asScala must_==List("Copper", "Silver", "Copper")
+    }
+
+    "transmit player events" in {
+      val networkPlayer = NetworkPlayer(noInput, output)
+      mockOtherPlayer.describe returns "another player"
+      networkPlayer.playerEvent(mockOtherPlayer, Discard, List(Copper(), Silver()))
+      val transmitted = Event.parseDelimitedFrom(toInput(output))
+      transmitted.getVerb() must_==Discard.present
+      transmitted.getPlayer() must_=="another player"
+      transmitted.getCardList.asScala must_==List("Copper", "Silver")
+    }
   }
 }
 
@@ -45,19 +89,21 @@ case class NetworkPlayer(private val input: InputStream, private val output: Out
   def describe() = "Network Player"
 
   def playerEvent(player: GenericPlayer[Any], action: Verb, cards: Seq[Card]) = {
-
+    Event.newBuilder.setPlayer(player.describe).setVerb(action.present).addAllCard(cards.map(_.describe).asJava).build.writeDelimitedTo(output)
   }
 
   def newHand(hand: Seq[Card]) = {
-
+    Hand.newBuilder.addAllCard(hand.map(_.describe).asJava).build.writeDelimitedTo(output)
   }
 
-  def query(question: Query) : Boolean = question match {
-    case BasicQuestion(str) => {
-      ProtobufQuery.newBuilder.setQuestion(str).build.writeDelimitedTo(output)
-      Answer.parseDelimitedFrom(input).getAnswer
-    }
-    case _ => false
+  private def sendQuery(question: Query) : Unit = question match {
+    case BasicQuestion(str) => ProtobufQuery.newBuilder.setQuestion(str).build.writeDelimitedTo(output)
+    case ChooseForOtherPlayer(cards, player, verb) => ProtobufChooseForOtherPlayer.newBuilder.addAllCard(cards.map(_.describe).asJava).setVerb(verb.present).setPlayer(player.describe).build.writeDelimitedTo(output)
+  }
+
+  def query(question: Query) : Boolean = {
+    sendQuery(question)
+    Answer.parseDelimitedFrom(input).getAnswer
   }
 
   def chooseFrom(cards: Seq[Card], purpose: Verb, minChoices: Int, maxChoices: Int) : Seq[Int] = {
