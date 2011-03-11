7a9fc4f182f64468d1c5e8b16b5bfb4d74fb8ca5
James Byatt <grumpyjames@gmail.com>
2011-03-11 19:21:44 +0000

    move completed action phase into main

---
+++ scalanion/src/main/scala/org/grumpysoft/ActionPhase.scala
@@ -0,0 +1,61 @@
+package org.grumpysoft
+
+import org.specs.mock.Mockito
+import org.specs.Specification
+import org.grumpysoft.TreasureCards._
+import org.grumpysoft.VictoryCards._
+
+object ActionPhase {
+
+  type Table = Seq[(Stacks, GenericPlayer[Card])]
+
+  private def isActionCard(c: Card) : Boolean = c match {
+    case a: ActionCard => true
+    case _ => false
+  }
+
+  private case class ActionExecution(counts: CountVonCount, stacks: Stacks, player: GenericPlayer[Card], supply: Supply, table: Table) {
+
+    def performActions() : ActionResult = counts.actions match {
+      case 0 => done
+      case _ => playAction()
+    }
+
+    private def chosenActionCard = {
+      val actionCards = stacks.hand.filter(isActionCard(_))
+      player.chooseFrom(actionCards, Play, 0, 1).headOption
+    }
+
+    private def done : ActionResult = {
+      ActionResult(counts, stacks, supply, table)
+    }
+
+    private def playAction() : ActionResult = {
+      chosenActionCard match {
+        case Some(a) => a match {
+          case b : ActionCard => executeAction(b)
+          case _ => throw new IllegalStateException("Something that wasn't an action card was chosen from a set of action cards.")
+        }
+        case None => done
+      }
+    }
+
+    private def executeAction(actionCard: ActionCard) : ActionResult = {
+      val actionResult = oneAction(actionCard)
+      ActionPhase(counts + actionResult.count.lessOneAction, actionResult.stacks, player, actionResult.supply, actionResult.table)
+    }
+
+    private def oneAction(actionCard: ActionCard) : ActionResult = {
+      table.map(_._2.playerEvent(player, Play, List(actionCard)))
+      actionCard.play(stacks.discardCard(actionCard), player, supply, table)
+    }
+
+  }
+
+  def apply(counts: CountVonCount, stacks: Stacks, player: GenericPlayer[Card], supply: Supply, table: Table) : ActionResult = {
+    ActionExecution(counts, stacks, player, supply, table).performActions
+  }
+
+}
+
+
