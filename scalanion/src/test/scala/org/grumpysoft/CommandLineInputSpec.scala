4402df9f5a60b4ccbdecbd588493faab3ce34653
James Byatt <grumpyjames@gmail.com>
2011-03-12 15:10:25 +0000

    convert another scalatest to specs

---
+++ scalanion/src/test/scala/org/grumpysoft/CommandLineInputSpec.scala
@@ -0,0 +1,17 @@
+package org.grumpysoft
+
+import java.io.BufferedReader
+import java.io.ByteArrayInputStream
+import java.io.InputStreamReader
+import org.specs.Specification
+
+class CommandLineInputSpec extends Specification {
+  "a streamed user input, when asked for an int" should {
+    "attempt to read, line by line, from the given stream, until a reasonable input is received" in {
+      val bytes = "aaga\r\nfsdfnlkn\n3\r\n".getBytes
+      val stream = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(bytes)))
+      val cli = new StreamedUserInput(stream)
+      cli.read must_==3
+    }
+  }
+}
