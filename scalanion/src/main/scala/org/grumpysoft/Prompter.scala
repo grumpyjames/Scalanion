4ffe4c883266c592501678110599d723a16d3046
James Byatt <grumpyjames@gmail.com>
2011-03-12 15:13:12 +0000

    undo some earlier madness with scoping packages like C++ namespaces

--- scalanion/src/main/scala/org/grumpysoft/Prompter.scala
+++ scalanion/src/main/scala/org/grumpysoft/Prompter.scala
@@ -1,43 +1,41 @@
-package org {
-  package grumpysoft {
-    class Prompter(input: UserInput, output: Printer) extends Promptable {
-      def prompt(greeting: SelfDescribing, options : Seq[SelfDescribing]) : Seq[Int] = {
-	uniquery.toSet(readNext(greeting, options).dropWhile({response => invalidReturn(options, response)}).head)
-      }
+package org.grumpysoft
 
-      def prompt(message: SelfDescribing) : Unit = {
-	output.println(message.describe)
-      }
-
-      private def invalidReturn(options : Seq[SelfDescribing], result : Seq[Int]) : Boolean = {
-	result.exists({one => one > options.size || one <= 0})
-      }
-
-      private def readNext(greeting: SelfDescribing, options: Seq[SelfDescribing]) : Stream[Seq[Int]] = {
-	doPrompt(greeting, options)
-	Stream.cons(input.read, readNext(greeting, options))
-      }
-      
-      private def doPrompt(greeting: SelfDescribing, options: Seq[SelfDescribing]) : Unit = {
-	output.println(greeting.describe)	
-	options.foreach(selfDescribing => output.println(selfDescribing.describe))
-      }
-    }
-
-    // TODO: this must be useful elsewhere! (and get rid of the reverse?)
-  object uniquery {
-    def toSet[A](stream : Seq[A]) : Seq[A] = {
-      val aList : List[A] = List()
-      stream.foldLeft(aList) ( (acc,el) =>
-        if (acc.contains(el)) acc else el :: acc
-      ).reverse
-    }
+class Prompter(input: UserInput, output: Printer) extends Promptable {
+  def prompt(greeting: SelfDescribing, options : Seq[SelfDescribing]) : Seq[Int] = {
+    uniquery.toSet(readNext(greeting, options).dropWhile({response => invalidReturn(options, response)}).head)
   }
 
-    class FormattedPrompter(input: UserInput, output: Printer, optionFormatter: Formatter) extends Prompter(input, output) with FormattedPrompts {
-      protected def formatter() : Formatter = {
-	optionFormatter
-      }
-    }
+  def prompt(message: SelfDescribing) : Unit = {
+    output.println(message.describe)
+  }
+
+  private def invalidReturn(options : Seq[SelfDescribing], result : Seq[Int]) : Boolean = {
+    result.exists({one => one > options.size || one <= 0})
+  }
+
+  private def readNext(greeting: SelfDescribing, options: Seq[SelfDescribing]) : Stream[Seq[Int]] = {
+    doPrompt(greeting, options)
+    Stream.cons(input.read, readNext(greeting, options))
+  }
+
+  private def doPrompt(greeting: SelfDescribing, options: Seq[SelfDescribing]) : Unit = {
+    output.println(greeting.describe)
+    options.foreach(selfDescribing => output.println(selfDescribing.describe))
+  }
+}
+
+// TODO: this must be useful elsewhere! (and get rid of the reverse?)
+object uniquery {
+  def toSet[A](stream : Seq[A]) : Seq[A] = {
+    val aList : List[A] = List()
+    stream.foldLeft(aList) ( (acc,el) =>
+      if (acc.contains(el)) acc else el :: acc
+    ).reverse
+  }
+}
+
+class FormattedPrompter(input: UserInput, output: Printer, optionFormatter: Formatter) extends Prompter(input, output) with FormattedPrompts {
+  protected def formatter() : Formatter = {
+    optionFormatter
   }
 }
