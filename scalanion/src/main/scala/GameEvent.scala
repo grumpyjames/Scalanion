e1188bec05c75374a3ed579dadd68351fd17e5ca
James Byatt <grumpyjames@gmail.com>
2011-03-20 11:43:42 +0000

    help intellij out a bit, convert command line player test to specs before adding game event tests.

--- scalanion/src/main/scala/GameEvent.scala
+++ scalanion/src/main/scala/GameEvent.scala
@@ -2,5 +2,5 @@ package org.grumpysoft
 
 sealed abstract class GameEvent
 
-case class Start(startTime: Long) extends GameEvent
-case class End(leaderBoard: Seq[(SelfDescribing, Int)]) extends GameEvent
+case class Start(startTime: Long) extends org.grumpysoft.GameEvent
+case class End(leaderBoard: Seq[(SelfDescribing, Int)]) extends org.grumpysoft.GameEvent
