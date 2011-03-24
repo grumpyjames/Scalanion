3577cb30cc031107fe326ace8c592f487158bbfc
James Byatt <grumpyjames@gmail.com>
2011-03-24 21:23:31 +0000

    when cards get added, tell the player whose hand is being added to (for council room too)

--- scalanion/src/main/scala/org/grumpysoft/actioncards/CouncilRoom.scala
+++ scalanion/src/main/scala/org/grumpysoft/actioncards/CouncilRoom.scala
@@ -3,8 +3,17 @@ package org.grumpysoft.actioncards
 import org.grumpysoft._
 
 case class CouncilRoom() extends ActionCardImpl {
+
+  private def addCard(a: StacksWithPlayer) : StacksWithPlayer = {
+    val newStacks = a._1.addCards(1)
+    a._2.newHand(newStacks.hand)
+    (newStacks, a._2)
+  }
+
   def play(stacks: Stacks, player: GenericPlayer[Card], supply: Supply, table: Table) : ActionResult = {
-    ActionResult.noTreasureOrActions(1, stacks.addCards(4), supply, table.map(a => (a._1.addCards(1), a._2)))
+    val newStacks = stacks.addCards(4)
+    player.newHand(newStacks.hand)
+    ActionResult.noTreasureOrActions(1, newStacks, supply, table.map(addCard(_)))
   }
 
   def describe() = "Council Room"
--- scalanion/src/test/scala/org/grumpysoft/actioncards/CouncilRoomSpec.scala
+++ scalanion/src/test/scala/org/grumpysoft/actioncards/CouncilRoomSpec.scala
@@ -11,8 +11,10 @@ object CouncilRoomSpec extends ActionCardSpecBase {
 
   val table = makeTable(playerTwoStacks, playerThreeStacks)
 
+  val actionResult = CouncilRoom().play(stacks, playerOne, supply, table)
+
   "council room" should {
-    val actionResult = CouncilRoom().play(stacks, playerOne, supply, table)
+
     "add four cards" in {
       actionResult.stacks.hand must_==(mixOfAllTypes.take(4) ++ twoCoppers)
     }
@@ -22,6 +24,11 @@ object CouncilRoomSpec extends ActionCardSpecBase {
     "add a card to each other player" in {
       actionResult.table.head._1.hand must_==witchAndDuchy.take(1)
       actionResult.table.last._1.hand must_==twoCoppers.take(1)
+    }
+    "tell each player what their hand now is" in {
+      there was one(playerOne).newHand(actionResult.stacks.hand)
+      there was one(playerTwo).newHand(actionResult.table.head._1.hand)
+      there was one(playerThree).newHand(actionResult.table.last._1.hand)
     }
   }
 
