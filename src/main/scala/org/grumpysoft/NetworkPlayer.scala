1fd5ee3d57cd135b998747fd930a4e6c8c017fe7
James Byatt <grumpyjames@gmail.com>
2012-02-21 21:10:38 +0000

    rely on inbuilt converters, do some Int and Integer boxing work. Compile now succeeds

--- src/main/scala/org/grumpysoft/NetworkPlayer.scala
+++ src/main/scala/org/grumpysoft/NetworkPlayer.scala
@@ -1,19 +1,19 @@
 package org.grumpysoft
 
-import scalaj.collection.Imports._
 import java.io.{OutputStream, InputStream}
 import org.grumpysoft.pb.Scalanion.{ServerToClient, Hand, Query => ProtobufQuery, ChooseFrom, ChooseForOtherPlayer => ProtobufChooseForOtherPlayer, GameEvent => PBGameEvent, Answer, Choices, Event, GameStart => PBStart, GameOver, PlayerWithScore}
+import scala.collection.JavaConversions._
 
 case class NetworkPlayer(private val name: String, private val input: InputStream, private val output: OutputStream) extends GenericPlayer[Int] {
   def describe() = name
 
-  def playerEvent(player: SelfDescribing, action: Verb, cards: Seq[Card]) = {
+  def playerEvent(player: SelfDescribing, action: Verb, cards: Seq[Card]) {
     if (!cards.isEmpty)
-      send(Event.newBuilder.setPlayer(player.describe).setVerb(action.present).addAllCard(cards.map(_.describe).asJava).build)
+      send(Event.newBuilder.setPlayer(player.describe()).setVerb(action.present).addAllCard(cards.map(_.describe())).build)
   }
 
-  def newHand(hand: Seq[Card]) = {
-    send(Hand.newBuilder.addAllCard(hand.map(_.describe).asJava).build)
+  def newHand(hand: Seq[Card]) {
+    send(Hand.newBuilder.addAllCard(hand.map(_.describe())).build)
   }
 
   def query(question: Query) : Boolean = {
@@ -22,59 +22,63 @@ case class NetworkPlayer(private val name: String, private val input: InputStrea
   }
 
   def chooseFrom(cards: Seq[Card], purpose: Verb, minChoices: Int, maxChoices: Int) : Seq[Int] = {
-    send(ChooseFrom.newBuilder.addAllCard(cards.map(_.describe).asJava).setVerb(purpose.present).setMinimumChoices(minChoices).setMaximumChoices(maxChoices).build)
-    Choices.parseDelimitedFrom(input).getChoiceList.asScala
+    send(ChooseFrom.newBuilder.addAllCard(cards.map(_.describe())).setVerb(purpose.present).setMinimumChoices(minChoices).setMaximumChoices(maxChoices).build)
+    Choices.parseDelimitedFrom(input).getChoiceList.map(_.intValue())
   }
 
-  private def sendQuery(question: Query) : Unit = question match {
-    case BasicQuestion(str) => sendWrappedQuery(str)
-    case ChooseForOtherPlayer(cards, player, verb) => sendWrappedQuery(cards, player, verb)
+  private def sendQuery(question: Query) {
+    question match {
+      case BasicQuestion(str) => sendWrappedQuery(str)
+      case ChooseForOtherPlayer(cards, player, verb) => sendWrappedQuery(cards, player, verb)
+    }
   }
 
-  private def sendWrappedQuery(rawQuery: String) : Unit = {
+  private def sendWrappedQuery(rawQuery: String) {
     send(ProtobufQuery.newBuilder.setQuestion(rawQuery).build)
   }
 
   private def makeCfop(cards: scala.Seq[Card], verb: Verb, player: SelfDescribing): ProtobufChooseForOtherPlayer = {
-    ProtobufChooseForOtherPlayer.newBuilder.addAllCard(cards.map(_.describe).asJava).setVerb(verb.present).setPlayer(player.describe).build
+    ProtobufChooseForOtherPlayer.newBuilder.addAllCard(cards.map(_.describe())).setVerb(verb.present).setPlayer(player.describe()).build
   }
 
-  private def sendWrappedQuery(cards: Seq[Card], player: SelfDescribing, verb: Verb) : Unit = {
+  private def sendWrappedQuery(cards: Seq[Card], player: SelfDescribing, verb: Verb) {
     send(ProtobufQuery.newBuilder
       .setChooseForOtherPlayer(makeCfop(cards, verb, player))
       .build)
   }
 
-  private def send(query: ProtobufQuery) : Unit = {
+  private def send(query: ProtobufQuery) {
     ServerToClient.newBuilder.setQuery(query).build.writeDelimitedTo(output)
   }
 
-  private def send(hand: Hand) : Unit = {
+  private def send(hand: Hand) {
     ServerToClient.newBuilder.setHand(hand).build.writeDelimitedTo(output)
   }
 
-  private def send(event: Event) : Unit = {
+  private def send(event: Event) {
     ServerToClient.newBuilder.setEvent(event).build.writeDelimitedTo(output)
   }
 
-  private def send(chooseFrom: ChooseFrom) : Unit = {
+  private def send(chooseFrom: ChooseFrom) {
     ServerToClient.newBuilder.setChooseFrom(chooseFrom).build.writeDelimitedTo(output)
   }
 
-  private def send(gameEvent: PBGameEvent) : Unit = {
+  private def send(gameEvent: PBGameEvent) {
     ServerToClient.newBuilder.setGameEvent(gameEvent).build.writeDelimitedTo(output)
   }
 
   private def buildGameOver(leaderboard: Seq[(SelfDescribing, Int)]) : GameOver = {
     val playerWithScores = leaderboard.map(pws =>
-      PlayerWithScore.newBuilder.setPlayerName(pws._1.describe).setScore(pws._2).build
-					 ).asJava
+      PlayerWithScore.newBuilder.setPlayerName(pws._1.describe()).setScore(pws._2).build
+					 )
     GameOver.newBuilder.addAllPlayerWithScore(playerWithScores).build      
   }
 
-  def gameEvent(event: GameEvent) : Unit = event match {
-    case Start(startTime) =>  send(PBGameEvent.newBuilder.setStart(PBStart.newBuilder.setStartTime(startTime)).build)
-    case End(leaderBoard) => send(PBGameEvent.newBuilder.setGameOver(buildGameOver(leaderBoard)).build)
+  def gameEvent(event: GameEvent) {
+    event match {
+      case Start(startTime) => send(PBGameEvent.newBuilder.setStart(PBStart.newBuilder.setStartTime(startTime)).build)
+      case End(leaderBoard) => send(PBGameEvent.newBuilder.setGameOver(buildGameOver(leaderBoard)).build)
+    }
   }
 }
 
--- src/main/scala/org/grumpysoft/RemotePlayer.scala
+++ src/main/scala/org/grumpysoft/RemotePlayer.scala
@@ -1,16 +1,16 @@
 package org.grumpysoft
 
-import scalaj.collection.Imports._
 import java.io.{InputStream, OutputStream}
 import collection.Seq
 import java.lang.String
 import collection.immutable.List
+import scala.collection.JavaConversions._
 import org.grumpysoft.pb.Scalanion._
 
 object RemotePlayer {
 
   def from(player: GenericPlayer[Int], input: InputStream, output: OutputStream) : RemotePlayer = {
-    Introduction.newBuilder.setPlayerName(player.describe).build.writeDelimitedTo(output)
+    Introduction.newBuilder.setPlayerName(player.describe()).build.writeDelimitedTo(output)
     RemotePlayer(player, input, output)
   }
 
@@ -18,54 +18,55 @@ object RemotePlayer {
 
 case class RemotePlayer(private val player: GenericPlayer[Int], private val input: InputStream, private val output: OutputStream) {
 
-  private def passEvent(message: ServerToClient): Unit = {
+  private def passEvent(message: ServerToClient) {
     val event = message.getEvent
-    val seq: Seq[String] = event.getCardList.asScala
+    val seq: Seq[String] = event.getCardList
     player.playerEvent(liftPlayer(event.getPlayer), liftVerb(event.getVerb), liftCards(seq))
   }
 
-  private def passHand(message: ServerToClient) : Unit = {
+  private def passHand(message: ServerToClient) {
     val hand = message.getHand
-    player.newHand(liftCards(hand.getCardList.asScala))
+    player.newHand(liftCards(hand.getCardList))
   }
 
-  private def passChooseFrom(message: ServerToClient) : Unit = {
+  private def passChooseFrom(message: ServerToClient) {
     val chooseFrom = message.getChooseFrom
-    val responses: scala.Seq[Int] = player.chooseFrom(liftCards(chooseFrom.getCardList.asScala), liftVerb(chooseFrom.getVerb), chooseFrom.getMinimumChoices, chooseFrom.getMaximumChoices)
-    Choices.newBuilder.addAllChoice(responses.asJava).build.writeDelimitedTo(output)
+    val responses: Seq[Int] = player.chooseFrom(liftCards(chooseFrom.getCardList),
+      liftVerb(chooseFrom.getVerb), chooseFrom.getMinimumChoices, chooseFrom.getMaximumChoices)
+    Choices.newBuilder.addAllChoice(responses.map(a => Int.box(a))).build.writeDelimitedTo(output)
   }
 
   private def sendResponse(response: Boolean): Any = {
     Answer.newBuilder.setAnswer(response).build.writeDelimitedTo(output)
   }
 
-  private def passQuery(message: ServerToClient) : Unit = {
+  private def passQuery(message: ServerToClient) {
     val query = message.getQuery
     if (query.hasQuestion) {
       sendResponse(player.query(BasicQuestion(query.getQuestion)))
     } else {
       val cfop = query.getChooseForOtherPlayer
-      sendResponse(player.query(ChooseForOtherPlayer(liftCards(cfop.getCardList.asScala), liftPlayer(cfop.getPlayer), liftVerb(cfop.getVerb))))
+      sendResponse(player.query(ChooseForOtherPlayer(liftCards(cfop.getCardList), liftPlayer(cfop.getPlayer), liftVerb(cfop.getVerb))))
     }
   }
 
-  private def passGameEvent(message: ServerToClient) : Unit = {
+  private def passGameEvent(message: ServerToClient) {
     val gameEvent = message.getGameEvent
     if (gameEvent.hasStart) {
       player.gameEvent(Start(gameEvent.getStart.getStartTime))
     } else {
-      val playersWithScores = gameEvent.getGameOver.getPlayerWithScoreList.asScala
+      val playersWithScores = gameEvent.getGameOver.getPlayerWithScoreList
       player.gameEvent(End(playersWithScores.map(a => (StringDescription(a.getPlayerName), a.getScore))))
     }
   }
 
-  def readAndForward : Unit = {
+  def readAndForward() {
     val message = ServerToClient.parseDelimitedFrom(input)
-    if (message.hasEvent) return passEvent(message)
-    if (message.hasHand) return passHand(message)
-    if (message.hasChooseFrom) return passChooseFrom(message)
-    if (message.hasQuery) return passQuery(message)
-    if (message.hasGameEvent) return passGameEvent(message)
+    if (message.hasEvent) passEvent(message)
+    if (message.hasHand) passHand(message)
+    if (message.hasChooseFrom) passChooseFrom(message)
+    if (message.hasQuery) passQuery(message)
+    if (message.hasGameEvent) passGameEvent(message)
   }
 
   private def liftPlayer(playerName: String) : SelfDescribing = {
