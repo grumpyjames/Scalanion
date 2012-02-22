0b502620824315c75fffa6a4e20f8d9ae64f04dd
James Byatt <grumpyjames@gmail.com>
2012-02-22 19:07:10 +0000

    fix network player spec by getting state in the right place, getting the implicits all lined up

--- src/test/scala/org/grumpysoft/NetworkPlayerSpec.scala
+++ src/test/scala/org/grumpysoft/NetworkPlayerSpec.scala
@@ -5,46 +5,48 @@ import scala.collection.JavaConversions._
 import java.io.{OutputStream, ByteArrayInputStream, InputStream, ByteArrayOutputStream}
 import org.grumpysoft.TreasureCards.{Copper, Silver}
 import org.specs2.mock.Mockito
-import org.grumpysoft.pb.Scalanion.{Hand, Query => ProtobufQuery, ServerToClient, ChooseForOtherPlayer => ProtobufChooseForOtherPlayer, Answer, Choices, Event}
+import org.grumpysoft.pb.Scalanion.{ServerToClient, Answer, Choices}
 
 object NetworkPlayerSpec extends Specification with Mockito {
 
-  val mockOtherPlayer = mock[GenericPlayer[Any]]
+  trait TestState extends org.specs2.specification.Scope {
+    val mockOtherPlayer = mock[GenericPlayer[Any]]
 
-  def buildInput(mutator: (OutputStream => Unit)) : InputStream = {
-    val scratchSpace = new ByteArrayOutputStream(128)
-    mutator(scratchSpace)
-    toInput(scratchSpace)
-  }
+    def buildInput(mutator: (OutputStream => Unit)) : InputStream = {
+      val scratchSpace = new ByteArrayOutputStream(128)
+      mutator(scratchSpace)
+      toInput(scratchSpace)
+    }
 
-  def toInput(baos: ByteArrayOutputStream) : InputStream = {
-    new ByteArrayInputStream(baos.toByteArray)
-  }
+    def toInput(baos: ByteArrayOutputStream) : InputStream = {
+      new ByteArrayInputStream(baos.toByteArray)
+    }
 
-  def buildResponsesInput() : InputStream = {
-    buildInput({scratchSpace =>
-      Choices.newBuilder.addChoice(0).addChoice(3).build.writeDelimitedTo(scratchSpace)
-      Answer.newBuilder.setAnswer(true).build.writeDelimitedTo(scratchSpace)
-    })
-  }
+    def buildResponsesInput() : InputStream = {
+      buildInput({scratchSpace =>
+        Choices.newBuilder.addChoice(0).addChoice(3).build.writeDelimitedTo(scratchSpace)
+        Answer.newBuilder.setAnswer(true).build.writeDelimitedTo(scratchSpace)
+      })
+    }
 
-  def falseResponseInput() : InputStream = {
-    buildInput(scratchSpace => Answer.newBuilder.setAnswer(false).build.writeDelimitedTo(scratchSpace))
-  }
+    def falseResponseInput() : InputStream = {
+      buildInput(scratchSpace => Answer.newBuilder.setAnswer(false).build.writeDelimitedTo(scratchSpace))
+    }
 
-  def noInput() : InputStream = {
-    buildInput(ss => {})
-  }
+    def noInput() : InputStream = {
+      buildInput(ss => {})
+    }
 
-  def makeNetworkPlayer(in: InputStream, out: OutputStream) = {
-    NetworkPlayer("test player", in, out)
+    def makeNetworkPlayer(in: InputStream, out: OutputStream) = {
+      NetworkPlayer("test player", in, out)
+    }
+
+    val output = new ByteArrayOutputStream(2048)
   }
 
   "network player" should {
-    val output = new ByteArrayOutputStream(2048)
-
-    "transmit two choices and retrieve two responses" in {
-      val networkPlayer = makeNetworkPlayer(buildResponsesInput, output)
+    "transmit two choices and retrieve two responses" in new TestState {
+      val networkPlayer = makeNetworkPlayer(buildResponsesInput(), output)
 
       networkPlayer.chooseFrom(List(Copper(), Silver(), Silver(), Copper()), Discard, 0, 2) must_==List(0, 3)
       networkPlayer.query(BasicQuestion("are you geoff?")) must_==true
@@ -52,47 +54,47 @@ object NetworkPlayerSpec extends Specification with Mockito {
       val networkPlayerOutput = toInput(output)
 
       val transmitted = ServerToClient.parseDelimitedFrom(networkPlayerOutput).getChooseFrom
-      transmitted.getCardList must_==List("Copper", "Silver", "Silver", "Copper")
-      transmitted.getMinimumChoices must_==0
-      transmitted.getMaximumChoices must_==2
-      transmitted.getVerb must_=="discard"
       val transmittedQuery = ServerToClient.parseDelimitedFrom(networkPlayerOutput).getQuery
-      transmittedQuery.getQuestion must_=="are you geoff?"
+      (transmitted.getCardList.map(a => a) must_==List("Copper", "Silver", "Silver", "Copper")) and
+        (transmitted.getMinimumChoices must_==0) and
+        (transmitted.getMaximumChoices must_==2) and
+        (transmitted.getVerb must_=="discard") and
+        (transmittedQuery.getQuestion must_=="are you geoff?")
     }
 
-    "transmit queries corresponding to choices for other players" in {
-      val networkPlayer = makeNetworkPlayer(falseResponseInput, output)
+    "transmit queries corresponding to choices for other players" in new TestState {
+      val networkPlayer = makeNetworkPlayer(falseResponseInput(), output)
       mockOtherPlayer.describe returns "another player"
       networkPlayer.query(ChooseForOtherPlayer(List(Copper(), Silver()), mockOtherPlayer, Gain)) must_==false
       val transmitted = ServerToClient.parseDelimitedFrom(toInput(output)).getQuery.getChooseForOtherPlayer
-      (transmitted.getPlayer must_=="another player") and (transmitted.getVerb must_=="gain") and (transmitted.getCardList must_==List("Copper", "Silver"))
+      (transmitted.getPlayer must_=="another player") and (transmitted.getVerb must_=="gain") and (transmitted.getCardList.map(a => a) must_==List("Copper", "Silver"))
     }
 
-    "transmit hand contents" in {
-      val networkPlayer = makeNetworkPlayer(noInput, output)
+    "transmit hand contents" in new TestState {
+      val networkPlayer = makeNetworkPlayer(noInput(), output)
       networkPlayer.newHand(List(Copper(), Silver(), Copper()))
       val transmitted = ServerToClient.parseDelimitedFrom(toInput(output)).getHand
-      transmitted.getCardList must_==List("Copper", "Silver", "Copper")
+      transmitted.getCardList.map(a => a) must_==List("Copper", "Silver", "Copper")
     }
 
     
-    "transmit player events" in {
-      val networkPlayer = makeNetworkPlayer(noInput, output)
+    "transmit player events" in new TestState {
+      val networkPlayer = makeNetworkPlayer(noInput(), output)
       mockOtherPlayer.describe returns "another player"
       networkPlayer.playerEvent(mockOtherPlayer, Discard, List(Copper(), Silver()))
       val transmitted = ServerToClient.parseDelimitedFrom(toInput(output)).getEvent
       (transmitted.getVerb must_==Discard.present) and (transmitted.getPlayer must_=="another player") and
-        (transmitted.getCardList must_==List("Copper", "Silver"))
+        (transmitted.getCardList.map(a => a) must_==List("Copper", "Silver"))
     }
 
-    "quietly swallow events concerning no cards" in {
+    "quietly swallow events concerning no cards" in new TestState {
       val networkPlayer = makeNetworkPlayer(noInput(), output)
       mockOtherPlayer.describe returns "moo"
       networkPlayer.playerEvent(mockOtherPlayer, Discard, Nil)
       output.toByteArray.length must_==0
     }
 
-    "transmit a start event" in {
+    "transmit a start event" in new TestState {
       val networkPlayer = makeNetworkPlayer(noInput(), output)
       val currentTime = System.currentTimeMillis()
       networkPlayer.gameEvent(Start(currentTime))
@@ -100,7 +102,7 @@ object NetworkPlayerSpec extends Specification with Mockito {
       transmitted.getStartTime must_==currentTime
     }
 
-    "transmit a game over event" in {
+    "transmit a game over event" in new TestState {
       val networkPlayer = makeNetworkPlayer(noInput(), output)
       val leaderBoard = List((StringDescription("foo"), 12), (StringDescription("bar"), 15))
       networkPlayer.gameEvent(End(leaderBoard))
