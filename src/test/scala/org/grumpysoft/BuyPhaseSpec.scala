128a32a9288045a30ed0c3d432a698cdbf07a0ce
James Byatt <grumpyjames@gmail.com>
2012-02-22 18:56:37 +0000

    buy phase spec fixed with new test state scope

--- src/test/scala/org/grumpysoft/BuyPhaseSpec.scala
+++ src/test/scala/org/grumpysoft/BuyPhaseSpec.scala
@@ -2,38 +2,41 @@ package org.grumpysoft
 
 import org.specs2.mutable.Specification
 import org.specs2.mock.Mockito
-
-import scala.collection.immutable.Stack
+import org.specs2.specification.Scope
 
 import TreasureCards._
 import VictoryCards._
 
 object BuyPhaseSpec extends Specification with Mockito {
 
-  val deck = List(Copper(), Copper(), Copper(), Estate(), Estate())
-  val hand = List(Copper(), Copper(), Copper(), Copper(), Estate())
-  val actionlessStacks = Stacks(deck, hand, List())
+  trait TestState extends Scope {
+    val deck = List(Copper(), Copper(), Copper(), Estate(), Estate())
+  
+    val hand = List(Copper(), Copper(), Copper(), Copper(), Estate())
+    val actionlessStacks = Stacks(deck, hand, List())
+  
+    val player = mock[GenericPlayer[Card]]
+    val supply = mock[Supply]
+  
+    val copperAndSilver = List(Copper(), Silver())
+    val justCopper = List(Copper())
+    val justSilver = List(Silver())
+  
+    val copperSupply = mock[Supply]
+    val emptySupply = mock[Supply]
+  }
 
-  val player = mock[GenericPlayer[Card]]
-  val supply = mock[Supply]
-
-  val copperAndSilver = List(Copper(), Silver())
-  val justCopper = List(Copper())
-  val justSilver = List(Silver())
-
-  val copperSupply = mock[Supply]
-  val emptySupply = mock[Supply]
   "the buy phase" should {
-    "immediately offer the actionless player buy choices from the supply" in {
+    "immediately offer the actionless player buy choices from the supply" in new TestState {
       supply.availableCards(4) returns copperAndSilver
       player.chooseFrom(copperAndSilver, Buy, 0, 1) returns justCopper
       supply.buy(Copper()) returns supply
-      val (newSupply, bought) = BuyPhase(1, 4, player, supply)
+      val (_, bought) = BuyPhase(1, 4, player, supply)
       bought must_==List(Copper())
     }
 
 
-    "offer the player a buy for each one available" in {
+    "offer the player a buy for each one available" in new TestState  {
       supply.availableCards(4) returns copperAndSilver
       supply.buy(Silver()) returns copperSupply
       copperSupply.availableCards(1) returns justCopper
@@ -44,7 +47,7 @@ object BuyPhaseSpec extends Specification with Mockito {
       bought must_==List(Copper(), Silver())
     }
 
-    "be ok with absolutely no buys" in {
+    "be ok with absolutely no buys" in new TestState {
       supply.availableCards(4) returns justCopper
       player.chooseFrom(justCopper, Buy, 0, 1) returns List()
       val (newSupply, bought) = BuyPhase(1, 4, player, supply)
