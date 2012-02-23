1da37cd01d54bd13b34e7cec284a85fd20e78ef2
James Byatt <grumpyjames@gmail.com>
2012-02-23 00:24:57 +0000

    fix chapel spec with scope trick

--- src/test/scala/org/grumpysoft/actioncards/ChapelSpec.scala
+++ src/test/scala/org/grumpysoft/actioncards/ChapelSpec.scala
@@ -3,25 +3,26 @@ package org.grumpysoft.actioncards
 import org.grumpysoft.{Trash, Stacks}
 import org.grumpysoft.TreasureCards.Copper
 
-object ChapelSpec extends ActionCardSpecBase {
+object ChapelSpec extends org.specs2.mutable.Specification {
 
-  val stacks = Stacks(List(), silverRemodelAndTwoCoppers, List())
+  trait TestData extends ActionCardSpecBase with org.specs2.specification.Scope {
+    val stacks = Stacks(List(), silverRemodelAndTwoCoppers, List())
+  }
 
   "chapel" should {
 
-    "offer the player the chance to trash up to four cards from their current hand" in {
+    "offer the player the chance to trash up to four cards from their current hand" in new TestData {
       playerOne.chooseFrom(silverRemodelAndTwoCoppers, Trash, 0, 4) returns List()
       val actionResult = Chapel().play(stacks, playerOne, supply, emptyTable)
-      actionResult.stacks must_==(stacks)
-      actionResult.treasure must_==0
+      (actionResult.stacks must_==(stacks)) and (actionResult.treasure must_==0)
     }
 
-    "trash the selected cards" in {
+    "trash the selected cards" in new TestData {
       playerOne.chooseFrom(silverRemodelAndTwoCoppers, Trash, 0, 4) returns copperAndSilver
       val actionResult = Chapel().play(stacks, playerOne, supply, eventOnlyTable)
-      actionResult.stacks.hand must_==List(Copper(), Remodel().toActionCard)
-      actionResult.treasure must_==0
-      checkEventReceived(playerOne, Trash, copperAndSilver, eventOnlyTable.map(_._2))
+      (actionResult.stacks.hand must_==List(Copper(), Remodel().toActionCard)) and
+        (actionResult.treasure must_==0) and
+        checkEventReceived(playerOne, Trash, copperAndSilver, eventOnlyTable.map(_._2))
     }
   }
 }
