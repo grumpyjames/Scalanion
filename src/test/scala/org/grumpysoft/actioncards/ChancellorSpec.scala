3a176eb92161d73ae27b0615fc705ddd252199a0
James Byatt <grumpyjames@gmail.com>
2012-02-26 10:37:53 +0000

    add test state trait to chancellor spec, fix the test

--- src/test/scala/org/grumpysoft/actioncards/ChancellorSpec.scala
+++ src/test/scala/org/grumpysoft/actioncards/ChancellorSpec.scala
@@ -2,14 +2,15 @@ package org.grumpysoft.actioncards
 
 import org.grumpysoft.{BasicQuestion, DeckDiscard, Stacks}
 
-object ChancellorSpec extends ActionCardSpecBase {
+object ChancellorSpec extends org.specs2.mutable.Specification {
 
-  val stacks = Stacks(threeCoppersAndAnEstate, twoCoppers, oneRemodel)
-
-  val discardYourDeck = BasicQuestion("do you want to discard your deck?")
+  trait TestState extends ActionCardSpecBase with org.specs2.specification.Scope {
+    val stacks = Stacks(threeCoppersAndAnEstate, twoCoppers, oneRemodel)
+    val discardYourDeck = BasicQuestion("do you want to discard your deck?")
+  }
 
   "chancellor" should {
-    "give player the option to discard their current deck, and do so if they request it" in {
+    "give player the option to discard their current deck, and do so if they request it" in new TestState {
       playerOne.query(discardYourDeck) returns true
       val actionResult = Chancellor().play(stacks, playerOne, supply, eventOnlyTable)
       actionResult.treasure must_==2
@@ -17,7 +18,7 @@ object ChancellorSpec extends ActionCardSpecBase {
       checkEventReceived(playerOne, DeckDiscard, threeCoppersAndAnEstate, eventOnlyTable.map(_._2))
     }
 
-    "just yield +2 treasure if deck is not elected for discard" in {
+    "just yield +2 treasure if deck is not elected for discard" in new TestState {
       playerOne.query(discardYourDeck) returns false
       val actionResult = Chancellor().play(stacks, playerOne, supply, eventOnlyTable)
       actionResult.treasure must_==2
